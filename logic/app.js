8e3500054bbac4fa1e1634e1d363b584bc4f79fb
Narcikss <mdgamedeveloper@gmail.com>
2022-05-15 01:48:14 +0200

    Add better sticky nav bar

--- logic/app.js
+++ logic/app.js
@@ -1,7 +1,13 @@
 const navArea = document.querySelector(".nav__area");
+const imgWrapper = document.querySelector(".img__wrapper");
+const checkVoyage = document.querySelector(".check__voyage");
 
 window.addEventListener("scroll", () => {
 	let y = window.scrollY;
-	if (y > 512) navArea.classList.add("sticky");
+	if (y > 513) navArea.classList.add("sticky");
 	else navArea.classList.remove("sticky");
+});
+
+imgWrapper.addEventListener("mouseover", (e) => {
+	checkVoyage.style.display = "block";
 });
