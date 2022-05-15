fbad0cdfe6c78988a53301a6aa77688a0a39c94e
Narcikss <mdgamedeveloper@gmail.com>
2022-05-15 21:51:31 +0200

    add header images move

--- logic/app.js
+++ logic/app.js
@@ -2,6 +2,10 @@ const navArea = document.querySelector(".nav__area");
 const imgWrapper = document.querySelector(".img__wrapper");
 const checkVoyage = document.querySelector(".check__voyage");
 
+const headerImage = document.querySelector(".header__ship");
+const HEADER_IMAGES_NUMBER = 4;
+let a = 0;
+
 window.addEventListener("scroll", () => {
 	let y = window.scrollY;
 	if (y > 513) navArea.classList.add("sticky");
@@ -16,3 +20,15 @@ function checkVoyageEvent(e) {
 	if (e.target.className == "header__ship") checkVoyage.style.display = "block";
 	else checkVoyage.style.display = "none";
 }
+
+function changeHeaderImage() {
+	if (a == 4) a = 0;
+
+	a++;
+	console.log(a);
+	headerImage.src = `images/ship-header-image-${a}.jpg`;
+}
+
+setInterval(() => {
+	changeHeaderImage();
+}, 3000);
